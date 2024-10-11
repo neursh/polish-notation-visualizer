8ec932018eb794c467bc4bdbad8302714c7c4108
neursh <neurs@spaark.click>
2024-10-11 21:20:18 +0700

    Add support buttons, remove all checks for user input

--- src/components/CharactersContainer.tsx
+++ src/components/CharactersContainer.tsx
@@ -1,9 +1,11 @@
 import { useHookstate } from '@hookstate/core';
+import { readText, writeText } from '@tauri-apps/plugin-clipboard-manager';
 import { motion } from 'framer-motion';
 import { memo, useCallback, useEffect, useMemo } from 'react';
 import AppContext from '../contextProvider';
 
 const CharactersContainer = memo(() => {
+  const total = useHookstate(AppContext.total);
   const keyList = useHookstate(AppContext.keyList);
   const result = useHookstate(AppContext.result);
   const displayingResult = useHookstate(AppContext.displayingResult);
@@ -19,7 +21,7 @@ const CharactersContainer = memo(() => {
   const highlight = useHookstate<number>(-1);
   const highlightColors = useMemo(() => ['green', 'green', 'red'], []);
 
-  const goThrough = useCallback(() => {
+  useEffect(() => {
     if (result.length > 0) {
       displayingResult.set(true);
       highlight.set(
@@ -28,8 +30,6 @@ const CharactersContainer = memo(() => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [result]);
-
-  useEffect(goThrough, [goThrough]);
 
   useEffect(() => {
     if (displayingResult.get()) {
@@ -83,10 +83,63 @@ const CharactersContainer = memo(() => {
                 .join(' ')
             : ''}
         </p>
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-4 pt-2">
+          {!displayingResult.get() && (
+            <>
+              <a
+                onClick={async () => {
+                  total.set(0);
+                  keyList.set([]);
+                }}
+              >
+                <motion.svg
+                  whileTap={{ scale: 1.25 }}
+                  className="h-5 w-5 outline-none"
+                  viewBox="0 -960 960 960"
+                  fill="black"
+                >
+                  <path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z" />
+                </motion.svg>
+              </a>
+              <a onClick={async () => await writeText(keyList.get().join(''))}>
+                <motion.svg
+                  whileTap={{ scale: 1.25 }}
+                  className="h-5 w-5 outline-none"
+                  viewBox="0 -960 960 960"
+                  fill="black"
+                >
+                  <path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z" />
+                </motion.svg>
+              </a>
+              <a
+                onClick={async () => {
+                  const text = (await readText()).split('');
+                  let current = 0;
+                  const total = text.length;
+                  const recall = () =>
+                    setTimeout(() => {
+                      if (current < total) {
+                        AppContext.addKey(text[current]);
+                        current++;
+                        recall();
+                      }
+                    }, 25);
+                  recall();
+                }}
+              >
+                <motion.svg
+                  whileTap={{ scale: 1.25 }}
+                  className="h-5 w-5 outline-none"
+                  viewBox="0 -960 960 960"
+                  fill="black"
+                >
+                  <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h167q11-35 43-57.5t70-22.5q40 0 71.5 22.5T594-840h166q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560h-80v120H280v-120h-80v560Zm280-560q17 0 28.5-11.5T520-800q0-17-11.5-28.5T480-840q-17 0-28.5 11.5T440-800q0 17 11.5 28.5T480-760Z" />
+                </motion.svg>
+              </a>
+            </>
+          )}
           {displayingResult.get() && (
             <a
-              className="pt-2"
               onClick={() =>
                 currentDisplayResult.get() > 0
                   ? currentDisplayResult.set((p) => p - 1)
@@ -97,7 +150,7 @@ const CharactersContainer = memo(() => {
             </a>
           )}
           <a
-            className="bg-black rounded-full mt-2 pt-2 pb-2 pl-4 pr-4 text-white"
+            className="bg-black rounded-full pt-2 pb-2 pl-4 pr-4 text-white"
             onClick={() =>
               !displayingResult.get()
                 ? AppContext.calculate()
--- src/contextProvider.tsx
+++ src/contextProvider.tsx
@@ -15,32 +15,6 @@ export default abstract class AppContext {
 
   static addKey(key: string) {
     if ('1234567890.+-*/ '.includes(key)) {
-      const keyList = AppContext.keyList.get();
-      const [previousKey] = keyList.slice(-1);
-
-      const isPreviousOperator = '+-*/'.includes(previousKey);
-      const isPreviousNumber = '1234567890'.includes(previousKey);
-      const isOperator = '+-*/'.includes(key);
-      const isNumber = '1234567890'.includes(key);
-
-      if (
-        (previousKey === key && !isOperator && !isNumber) ||
-        (!previousKey && !isNumber) ||
-        (key === '.' && !isPreviousNumber)
-      ) {
-        return;
-      }
-
-      if (
-        (isPreviousOperator && isNumber) ||
-        (isPreviousNumber && isOperator) ||
-        (isPreviousOperator && isOperator)
-      ) {
-        AppContext.total.set((p) => p + 2);
-        AppContext.keyList.merge([' ', key]);
-        return;
-      }
-
       AppContext.total.set((p) => p + 1);
       AppContext.keyList[AppContext.keyList.length].set(key);
       return;
